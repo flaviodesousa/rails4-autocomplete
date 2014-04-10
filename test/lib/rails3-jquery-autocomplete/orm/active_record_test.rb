5577bff81ce4e9bc47c26827f8588b513a22ddba
Flavio de Sousa <fl@viodesousa.com>
2014-04-10 05:52:06 -0300

    Avoiding method #all not defined error on test

--- test/lib/rails3-jquery-autocomplete/orm/active_record_test.rb
+++ test/lib/rails3-jquery-autocomplete/orm/active_record_test.rb
@@ -50,6 +50,7 @@ module Rails4Autocomplete
           mock(self).get_autocomplete_where_clause(model, term, method, {}) { ["WHERE something"] }
           mock(model).table_name.times(any_times) { 'model_table_name' }
 
+          mock(model).all { model }
           mock(model).scoped { model }
           mock(model).select(["field"]) { model }
           mock(model).where(["WHERE something"]).mock!.limit(10).mock!.
